9d865ca4ce44032aa8e091e7ef68c435fd5f4977
Antoine Bollengier <44288655+b5i@users.noreply.github.com>
2024-11-23 10:59:59 +0100

    Initial Commit

---
+++ Camera/CameraApp.swift
@@ -0,0 +1,21 @@
+//
+//  CameraApp.swift
+//  Camera
+//
+//  Created by Antoine Bollengier on 23.11.2024.
+//  Copyright © 2024 Antoine Bollengier (github.com/b5i). All rights reserved.
+//  
+
+import SwiftUI
+
+@main
+struct CameraApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
